01e8a77ebaf082ee4a31f3258f9fb945a6e93af3
SpacecapsAlex <spacecapsalex@hotmail.com>
2023-01-21 21:46:38 +0300

    Added database

---
+++ dsKnowledgeTest/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dsKnowledgeTest.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
